65b0d183a0fb83d5afa0c81a4b30f269889d45b5
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-01 15:19:55 -0500

    make package install work

---
+++ gogpy/__init__.py
@@ -0,0 +1,1 @@
+from .gogpy import gog
--- setup.py
+++ setup.py
@@ -4,12 +4,13 @@ with open('README.rst') as file:
     long_description = file.read()
 
 setup(name='gogpy',
+      packages=['gogpy'],
       description='use gog visualization system from Python',
       long_description=long_description,
       license='MIT',
       author='Aaron Schumacher',
       author_email='ajschumacher@gmail.com',
       url='https://github.com/ajschumacher/gogpy',
-      download_url='https://github.com/ajschumacher/gogpy/tarball/0.0.1',
-      version='0.0.1',
+      download_url='https://github.com/ajschumacher/gogpy/tarball/0.0.2',
+      version='0.0.2',
       install_requires=['requests'])
